1b24d4d8320a87842ef1b2cf400c93d34b0d6c0b
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-09-22 17:22:56 +0200

    to do button

---
+++ src/components/ToDoButton.vue
@@ -0,0 +1,15 @@
+<template>
+    <button>
+        <slot>Create</slot>
+    </button>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+button {
+    padding: 8px 16px; 
+    border: none;
+    }
+</style>
--- src/components/ToDoCreator.vue
+++ src/components/ToDoCreator.vue
@@ -1,9 +1,8 @@
 <template>
-    <div class="input-wrap">
+    <div class="input-wrap" :class="{'input-err' : todoState.invalid}">
         <input type="text" v-model="todoState.todo" />
-        <button @click="createToDo()">Create</button>
-        
-
+        <!-- <button @click="createToDo()">Create</button> --> 
+        <ToDoButton @click="createToDo()">Let's create</ToDoButton>
     </div>
         <pre>{{ todoState.todo }}</pre>
 
@@ -13,9 +12,13 @@
 
 <script>
 import { reactive, ref, defineEmits } from "vue";
+import ToDoButton from "./ToDoButton.vue";
 const emit = defineEmits(["createToDo"]);
 
 export default {
+     components: {
+        ToDoButton, // Register ToDoButton component
+    },
     data() {
         return {
             todoState: reactive({
@@ -46,28 +49,39 @@ export default {
 
 <style lang="scss" scoped>
 .input-wrap {
-    display: flex;
-    transition: 250ms ease;
-    border: 2px solid #41b080;
+  display: flex;
+  transition: 250ms ease;
+  border: 2px solid #41b080;
 
-    &:focus-within {
-        box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
-            0 -2px 4px -2px rgb(0 0 0 / 0.1);
+  &.input-err {
+    border-color: red;
+  }
+
+  &:focus-within {
+    box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
+      0 -2px 4px -2px rgb(0 0 0 / 0.1);
+  }
+
+  input {
+    width: 100%;
+    padding: 8px 6px;
+    border: none;
+
+    &:focus {
+      outline: none;
     }
+  }
 
-    input {
-        width: 100%;
-        padding: 8px 6px;
-        border: none;
+//   button {
+//     padding: 8px 16px;
+//     border: none;
+//   }
+}
 
-        &:focus {
-            outline: none;
-        }
-    }
-
-    button {
-        padding: 8px 16px;
-        border: none;
-    }
+.err-msg {
+  margin-top: 6px;
+  font-size: 12px;
+  text-align: center;
+  color: red;
 }
 </style>
